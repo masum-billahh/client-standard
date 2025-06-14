a874914480444fba1b4171763794aa5ed6d457f4
codersmutant <masumb9191@gmail.com>
2025-06-14 21:33:36 +0600

    added iframe load failed error

--- templates/iframe-container.php
+++ templates/iframe-container.php
@@ -18,7 +18,7 @@ if (!defined('ABSPATH')) {
             scrolling="no"
             sandbox="allow-scripts allow-forms allow-popups allow-popups-to-escape-sandbox allow-same-origin"
             referrerpolicy="no-referrer"
-            style="width: 100%; min-height: 150px; border: none; overflow: hidden;"
+            style="width: 100%; height: 50px; border: none; overflow: hidden;"
         ></iframe>
     </div>
     <div id="wpppc-paypal-message" class="wpppc-message" style="display: none;"></div>
@@ -28,3 +28,28 @@ if (!defined('ABSPATH')) {
         <div class="wpppc-loading-text"><?php _e('Processing payment...', 'woo-paypal-proxy-client'); ?></div>
     </div>
 </div>
+
+
+<script>
+jQuery(function ($) {
+    let iframe = $('#paypal-proxy-iframe');
+    let something = $('#paypal-buttons-container');
+
+    // Success: iframe loads
+    iframe.on('load', function () {
+        if (something.length) {
+            iframe.css('height', '150px');
+            console.log('Content available, height increased.');
+        } 
+    });
+
+    // Fallback: if not loaded in 1 seconds
+    setTimeout(function () {
+        if (!iframe[0].contentWindow || iframe[0].contentWindow.length === 0) {
+            console.log('Iframe failed or blocked.');
+            iframe.css('height', '50px'); 
+            $('<div style="color:red; padding-top:10px; text-align:center;">Sorry, something went wrong. Please refresh the page or contact the site owner.</div>').insertAfter(iframe);
+        }
+    }, 2000);
+});
+</script>
